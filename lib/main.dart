3be04611b3b3b959df2f7d1fe463c2d5c67505d1
Nirmal Yohannan <n.yohannan@avua.com>
2024-06-15 10:21:31 +0530

    -added app icon -set minimum window size -set window title

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:bloatware_buster/view/home_screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  //needed to ensure binding was initialized
+  WidgetsFlutterBinding.ensureInitialized();
+  await setWindowProperties();
   runApp(const MyApp());
+}
+
+Future<void> setWindowProperties() async {
+  await WindowManager.instance.ensureInitialized();
+  windowManager.waitUntilReadyToShow().then((_) async {
+    await windowManager.setTitle('Bloatware Buster');
+    await windowManager.setMinimumSize(const Size(800, 600));
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -13,7 +25,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Bloatware Buster',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
       home: const HomeScreen(),
